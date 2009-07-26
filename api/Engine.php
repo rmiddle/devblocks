30ce7351ce4328a378fb8a396619498edae5313b
rmiddle <robert@middleswarth.net>
2009-07-26 12:21:32 -0400

    Revert "* [Platform] URL Service now has a $check_proxy argument in the write() method which can be used to situationally ignore the proxy when writing URLs.  This is useful when you need to have a proxy tool (like the Support Center in Cerb4) write a URL  for back-end app (such as Worker Notifications in Cerb4 triggered from SC behavior). The default is to act like Devblocks has always acted (auto-context on writing URLs).  A new DEVBLOCKS_APP_WEBPATH constant will always provide the back-end web path (when you absolutely need it).  The DEVBLOCKS_WEBPATH constant will always be contextual."
    This reverts commit 39eceb6026c231da25281588ac80edbc5dd05a27.
    
    Remove this patch since it was added since 4.2.2 was released.  That will make this the true 4.2.2 branch

--- api/Engine.php
+++ api/Engine.php
@@ -2291,16 +2291,14 @@ class _DevblocksUrlManager {
 		return $parts;
 	}
 	
-	function write($sQuery='',$full=false,$check_proxy=true) {
-		$args = $this->parseQueryString($sQuery);
+	function write($sQuery='',$full=false) {
+		$url = DevblocksPlatform::getUrlService();
+		$args = $url->parseQueryString($sQuery);
 		$c = @$args['c'];
 		
-		// Allow proxy override
-		if($check_proxy) {
-    		@$proxyssl = $_SERVER['HTTP_DEVBLOCKSPROXYSSL'];
-    		@$proxyhost = $_SERVER['HTTP_DEVBLOCKSPROXYHOST'];
-    		@$proxybase = $_SERVER['HTTP_DEVBLOCKSPROXYBASE'];
-		}
+	    @$proxyssl = $_SERVER['HTTP_DEVBLOCKSPROXYSSL'];
+	    @$proxyhost = $_SERVER['HTTP_DEVBLOCKSPROXYHOST'];
+	    @$proxybase = $_SERVER['HTTP_DEVBLOCKSPROXYBASE'];
 
 		// Proxy (Community Tool)
 		if(!empty($proxyhost)) {
@@ -2342,10 +2340,10 @@ class _DevblocksUrlManager {
 				$prefix = sprintf("%s://%s%s",
 					($this->isSSL() ? 'https' : 'http'),
 					$_SERVER['HTTP_HOST'],
-					DEVBLOCKS_APP_WEBPATH
+					DEVBLOCKS_WEBPATH
 				);
 			} else {
-				$prefix = DEVBLOCKS_APP_WEBPATH;
+				$prefix = DEVBLOCKS_WEBPATH;
 			}
 
 			// Index page
